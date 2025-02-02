90c771726c1cbd334adb57997f6c6fbf9f071b6c
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-02-01 23:36:24 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -5,3 +5,14 @@ prices.push(79.99);//added a new price
 prices.shift (); //remove the first price
 
 console.log("Updated Prices:", prices);
+
+//Task 2: Customer Order
+let orders = [4, 5, 6, 7, 8];
+
+orders[2] += 5; //increasing the third order by 5
+ let total = orders.reduce((sum, order) => sum +order, 0);
+
+ console.log("Updated Order:", orders);
+ console.log("Total Orders:", total);//Total: 35
+
+ //Task 3: Employee Performance Tracking
